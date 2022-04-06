b1789ba4ebe3c693506adafe92df81e1362882d2
Abdul Rafay <99marafay@gmail.com>
2022-04-06 21:34:25 +0500

    Adding Moment

--- The Runner/The Runner/Assets/Script/PlayerMoment.cs
+++ The Runner/The Runner/Assets/Script/PlayerMoment.cs
@@ -4,20 +4,60 @@ using UnityEngine;
 
 public class PlayerMoment : MonoBehaviour
 {
+    //variable that will store the compoents data.
+    private Rigidbody2D rb;
+
+
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        //this will display the messgae in the console log
-        //Debug.Log("Hwllow!!");
+        //Gett the compoent and storing it into the variable.
+        //if it is needed then it can be reused.
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        //Movment Function.
+        movement();
+
+    }
+    private void jumpmovement()
+    {
+        //Jump Code for the character
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector2(0, 14f);
         }
-        
+    }
+    private void rightleftmovement()
+    {
+        //horizontial Input
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //vertical Input
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        //7f is the speed of the character.
+        //rb.velocity is the axis of the charter where the charter will move.
+        rb.velocity = new Vector2(horizontalInput * 7f, rb.velocity.y);
+
+        //rb.velocity = new Vector2(verticalInput * 7f, rb.velocity.x);
+
+        //rb.velocity= new Vector2(verticalInput *)
+
+
+        // Sir Provided Code
+        // Transforming thre object
+        //transform.Translate(new Vector2(horizontalInput, verticalInput));
+    }
+    private void movement()
+    {
+
+        jumpmovement();
+        rightleftmovement();
+
     }
 }
